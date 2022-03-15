6c617a3e6919b50ae7e832dd07549063609b8dac
James Prentice <jamesprenticez@gmail.com>
2022-03-15 16:18:04 +1300

    returning a python file

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,22 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const {spawn} = require('child_process');
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+    let python = spawn('python', ['python/hello.py']);
+    let dataToSend = '';
+
+    python.stdout.on("data", (data) => {
+      //console.log(data)
+      dataToSend += data.toString();
+    })
+
+    python.stderr.on('data', (data) => {
+      console.error(data.toString());
+    });
+ 
+    python.on("exit", (code) => {
+    console.log(dataToSend)
+    res.status(200).json({ message: dataToSend});
+  })
 }
+
+//https://stackoverflow.com/questions/60684227/api-resolved-without-sending-a-response-in-nextjs
