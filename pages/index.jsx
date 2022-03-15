fa3063854e2fa5157047aad2c08a63db2690a89c
James Prentice <jamesprenticez@gmail.com>
2022-03-16 11:16:38 +1300

    readable stream?

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ export default function Home({data}) {
     const res = await fetch('https://pushbutton.vercel.app/api')
     //const res = await fetch('http://localhost:3000/api/ip')
     console.log(res)
-    const data = await res.json()
+    const data = await res
     console.log(data)
   }
 
@@ -32,6 +32,45 @@ export default function Home({data}) {
   const data = await res.json()
   console.log(data)
 }
+
+fetch('https://pushbutton.vercel.app/api')
+.then(response => response.body)
+.then(rb => {
+  const reader = rb.getReader();
+
+  return new ReadableStream({
+    start(controller) {
+      // The following function handles each data chunk
+      function push() {
+        // "done" is a Boolean and value a "Uint8Array"
+        reader.read().then( ({done, value}) => {
+          // If there is no more data to read
+          if (done) {
+            console.log('done', done);
+            controller.close();
+            return;
+          }
+          // Get the data and send it to the browser via the controller
+          controller.enqueue(value);
+          // Check chunks by logging to the console
+          console.log(done, value);
+          push();
+        })
+      }
+
+      push();
+    }
+  });
+})
+.then(stream => {
+  // Respond with our stream
+  return new Response(stream, { headers: { "Content-Type": "text/html" } }).text();
+})
+.then(result => {
+  // Do things with result
+  console.log("readable stream?");
+  console.log(result);
+});
 
   return (
     <>
