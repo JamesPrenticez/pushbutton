e272ecb06851899d75752bbbe24ad576ddf678e4
James Prentice <jamesprenticez@gmail.com>
2022-03-26 11:20:45 +1300

    dump

--- api/date.py
+++ api/date.py
@@ -1,8 +1,8 @@
 from http.server import BaseHTTPRequestHandler
 import datetime
-
+import json 
 class handler(BaseHTTPRequestHandler):
   def date(request):
       now = datetime.datetime.now() 
       msg = f'Today is {now}'
-      return msg
+      return json.dump(msg)
