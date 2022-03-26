b88a4bd770dd603786aa8d84120cc16c1a9467cc
James Prentice <jamesprenticez@gmail.com>
2022-03-26 20:27:46 +1300

    Simple1

--- pages/readStream.jsx
+++ pages/readStream.jsx
@@ -5,54 +5,54 @@ import Header from "../components/Header";
 export default function ReadStream() {
   const [result, setResult] = useState("");
   
-  // async function handleClick(){
-  //   try {
-  //     fetch("https://pushbutton.vercel.app/api/date")
-  //       .then((response) => response.body)
-  //       .then((rb) => {
-  //         const reader = rb.getReader();
+  function handleClick(){
+    try {
+      fetch("https://pushbutton.vercel.app/api/date")
+        .then((response) => response.body)
+        .then((rb) => {
+          const reader = rb.getReader();
 
-  //         return new ReadableStream({
-  //           start(controller) {
-  //             // The following function handles each data chunk
-  //             function push() {
-  //               // "done" is a Boolean and value a "Uint8Array"
-  //               reader.read().then(({ done, value }) => {
-  //                 // If there is no more data to read
-  //                 if (done) {
-  //                   console.log("done", done);
-  //                   controller.close();
-  //                   return;
-  //                 }
-  //                 // Get the data and send it to the browser via the controller
-  //                 controller.enqueue(value);
-  //                 // Check chunks by logging to the console
-  //                 console.log(done, value);
-  //                 push();
-  //               });
-  //             }
+          return new ReadableStream({
+            start(controller) {
+              // The following function handles each data chunk
+              function push() {
+                // "done" is a Boolean and value a "Uint8Array"
+                reader.read().then(({ done, value }) => {
+                  // If there is no more data to read
+                  if (done) {
+                    console.log("done", done);
+                    controller.close();
+                    return;
+                  }
+                  // Get the data and send it to the browser via the controller
+                  controller.enqueue(value);
+                  // Check chunks by logging to the console
+                  console.log(done, value);
+                  push();
+                });
+              }
 
-  //             push();
-  //           },
-  //         });
-  //       })
-  //       .then((stream) => {
-  //         // Respond with our stream
-  //         return new Response(stream, {
-  //           headers: { "Content-Type": "text/html" },
-  //         }).text();
-  //       })
-  //       .then((result) => {
-  //         // Do things with result
-  //         console.log("readable stream???????????");
-  //         console.log(result);
-  //         console.log(response);
-  //         setResult(result);
-  //       });
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
+              push();
+            },
+          });
+        })
+        .then((stream) => {
+          // Respond with our stream
+          return new Response(stream, {
+            headers: { "Content-Type": "text/html" },
+          }).text();
+        })
+        .then((result) => {
+          // Do things with result
+          console.log("readable stream???????????");
+          console.log(result);
+          console.log(response);
+          setResult(result);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <>
--- pages/simple.jsx
+++ pages/simple.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from "next/head"
 import Header from "../components/Header"
 
 export default function Simple() {
+  const [result, setResult] = useState("");
+
+  function handleClick(){
+    setResult("test")
+  }
+
   return (
     <>
       <Head>
@@ -11,7 +17,25 @@ export default function Simple() {
 
       <Header />
 
-      <h1>Simple</h1>
+      <div className="flex justify-center">
+        <main className="w-full max-w-3xl xl:max-w-7xl space-y-4">
+          <h1>Quick Maths</h1>
+
+          <div className="flex justify-between bg-[#21262b] cursor-pointer p-4 text-white">
+            <div className="">
+              <p>LocalHost AXIOS = {result}</p>
+            </div>
+            <div className="ml-auto ">
+              <button
+                className="bg-[#007FFF] p-4"
+                onClick={() => handleClick()}
+              >
+                FETCH
+              </button>
+            </div>
+          </div>
+        </main>
+      </div>
 
     </>
   )
